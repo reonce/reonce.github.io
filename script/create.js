ca2b235dbe910c41f5138a6b8a415367187cf844
jaydonyin <1303491119@qq.com>
2022-05-04 20:53:16 +0800

    synchronization

--- script/create.js
+++ script/create.js
@@ -1,71 +1,93 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const { tagOptions, pathOptions } = require("./dataSource");
 
-typeInput();
-
-function typeInput() {
-  /**如果直接调用 yarn new 【文章名】 */
-  const title = process.argv[2];
-  if (title) {
-    createFile(title, []);
-    return;
+class NewArticle {
+  constructor() {
+    this.title = "";
+    this.tags = [];
+    this.folderPath = "";
   }
-  inquirer
-    .prompt([
-      {
-        name: "input",
-        type: "input",
-        message: "输入你想新建的文章标题",
-        default: "未命名文件",
-      },
-    ])
-    .then((res) => {
-      typeCheckbox(res.input);
-    });
-}
-/**选择标签 */
-function typeCheckbox(title) {
-  inquirer
-    .prompt([
-      {
-        name: "checkbox",
-        type: "checkbox",
-        message: "选择文章标签",
-        choices: [
-          "JavaScript",
-          "TypeScript",
-          "Array",
-          "Object",
-          "React",
-          "浏览器工作原理",
-          "红宝书",
-          "感悟",
-        ],
-        default: [""],
-      },
-    ])
-    .then((res) => {
-      createFile(title, res.checkbox);
-    })
-    .catch((err) => {
-      console(err);
-    });
-}
-
-function createFile(title, tags) {
-  const date = new Date();
-  const formatDate = `${date.getFullYear()}-${
-    date.getMonth() + 1
-  }-${date.getDate()}`;
-  const temp = `---
-title: ${title}
+  /**输入文章标题 */
+  articleInput = () => {
+    /**如果直接调用 yarn new 【文章名】 */
+    const title = process.argv[2];
+    if (title) {
+      this.title = title;
+      createFile();
+      return;
+    }
+    inquirer
+      .prompt([
+        {
+          name: "input",
+          type: "input",
+          message: "输入你想新建的文章标题",
+          default: "未命名文件",
+        },
+      ])
+      .then((res) => {
+        this.title = res.input;
+        this.tagCheckbox();
+      });
+  };
+  /**选择文章标签 */
+  tagCheckbox = () => {
+    inquirer
+      .prompt([
+        {
+          name: "tagCheckbox",
+          type: "checkbox",
+          message: "选择文章标签",
+          choices: tagOptions,
+          default: [""],
+        },
+      ])
+      .then((res) => {
+        this.tags = res.tagCheckbox;
+        this.folderPathCheckbox();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  folderPathCheckbox = () => {
+    inquirer
+      .prompt([
+        {
+          name: "folderPathCheckbox",
+          type: "list",
+          message: "选择文章所在文件夹路径",
+          choices: pathOptions,
+          default: [""],
+        },
+      ])
+      .then((res) => {
+        this.folderPath = res.folderPathCheckbox;
+        this.createFile();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  /**创建文件 */
+  createFile = () => {
+    const date = new Date();
+    const formatDate = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}`;
+    const temp = `---
+title: ${this.title}
 date: ${formatDate}
 author: reonce
-tags: ${[...tags]}
+tags: ${[...this.tags]}
 location: 成都  
 ---
 `;
-  const newPath = `../docs/_posts/${title}.md`;
-  fs.writeFileSync(path.resolve("__dirname", newPath), temp);
+    const newPath = `../docs/_posts/${this.folderPath}/${this.title}.md`;
+    fs.writeFileSync(path.resolve("__dirname", newPath), temp);
+  };
 }
+
+new NewArticle().articleInput();
---
+++ script/dataSource.js
@@ -0,0 +1,22 @@
+const tagOptions = [
+  "JavaScript",
+  "TypeScript",
+  "Array",
+  "Object",
+  "React",
+  "浏览器工作原理",
+  "红宝书",
+  "感悟",
+];
+
+const pathOptions = [
+  "",
+  "JavaScript属性和方法",
+  "浏览器工作原理",
+  "JS高级程序设计",
+];
+
+module.exports = {
+  tagOptions,
+  pathOptions,
+};
