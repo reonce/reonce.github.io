5c36d96516d4315c9b880f1692902ef853716b53
jaydonyin <1303491119@qq.com>
2022-05-04 21:14:26 +0800

    synchronization

--- script/dataSource.js
+++ script/dataSource.js
@@ -1,10 +1,12 @@
 const tagOptions = [
   "JavaScript",
+  "css",
   "TypeScript",
   "Array",
   "Object",
   "React",
   "浏览器工作原理",
+  "前端工程化",
   "红宝书",
   "感悟",
 ];
@@ -14,6 +16,7 @@ const pathOptions = [
   "JavaScript属性和方法",
   "浏览器工作原理",
   "JS高级程序设计",
+  "前端工程化",
 ];
 
 module.exports = {
