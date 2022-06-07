e7e6771f67c7e9743fbb3f8a7d4840854471fd63
reonce <jaydon926@gmail.com>
2022-06-07 23:31:51 +0800

    synchronization

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: "@vuepress/blog",
   title: "reonce的前端博客",
   description: "欢迎来访，这是一个前端博客",
-  logo: "../public/logo.png",
+  logo: "./public/logo.png",
   head: [["标签", { rel: "icon", href: "/favicon.ico" }]],
   evergreen: true, // 不需要兼容杂牌浏览器
   markdown: {
